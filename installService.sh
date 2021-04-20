b91f56df31611451b3c23c1dad209b82ade53b47
Henrique Felix <hfelixmiguell@gmail.com>
2021-04-20 01:39:14 +0100

    comentarios para installService.sh

--- installService.sh
+++ installService.sh
@@ -2,3 +2,7 @@ docker-compose build && docker-compose up -d && docker exec -it nodejs expo init
 
 ## IF YOU NEED GET ACCESS ROOT TO YOUR NODEJS CONTAINER, RUN ON TERMINAL : 
 # docker exec -itu root {containerName} passwd
+
+
+## IF YOU HAS PROBLEM WITH EXPO , CHANGE ON CONTAINER THE EXPO IP
+# export REACT_NATIVE_PACKAGER_HOSTNAME=192.168.X.XXX
